7e70078fa016c20f1b258d5564a3e282ba650ba6
Monika Nusi <c00285423@setu.ie>
2025-09-15 11:56:53 +0100

    Persistence filter added

---
+++ src/lib/filters/persistance.ts
@@ -0,0 +1,22 @@
+/*perstistance - the number of steps it takes to reduce it to a single digit by repeatedly multplying its digits*/
+
+export function persistance(n: number): number {
+  let count = 0;
+  let value = n;
+// repeat while number has more then one digit
+while (value >= 10){
+   value = value
+    .toString() //conver number to string
+    .split('') //split into digits
+    .map(Number) //conver each digit back to number
+    .reduce((acc, d)=> acc * d, 1) //multiply all digits togerther
+   count ++;
+
+}
+return count;
+}
+
+//map over a sequence of numbers
+export function persistances(nums: number[]): number[]{
+    return nums.map(persistance);
+}
---
+++ src/lib/filters/persistence.test.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from 'vitest';
+import { persistances } from './persistance';
+
+describe('Persistance filter', () => {
+  it('the number of steps it takes to reduce it to a single digit by repeatedly multplying its digits', () => {
+    const input = [39,999,4,25];
+    const expected = [3,4,0,2];
+    expect(persistances(input)).toEqual(expected);
+  });
+  
+  
+});
